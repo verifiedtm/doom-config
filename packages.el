0394b61e8270c731ccdfb64f785b05d2f6cfdf3f
Timothy Miller <86238857+verifiedtm@users.noreply.github.com>
2025-01-22 10:48:27 -0500

    Add just-mode to packages.el

--- packages.el
+++ packages.el
@@ -6,6 +6,7 @@
 (package! prettier-js)
 (package! persistent-soft)
 (package! ellama)
+(package! just-mode)
 
 (when (file-exists-p! "packages-local.el" doom-user-dir)
   (load! "packages-local.el" doom-user-dir))
